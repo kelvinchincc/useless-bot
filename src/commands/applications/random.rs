bd24ed0ca1edf5a64af74e447e498a03fbf37b51
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-06-05 22:38:26 +0800

    Add choose command to random application Implement random choice functionality

--- src/commands/applications/random.rs
+++ src/commands/applications/random.rs
@@ -10,7 +10,7 @@ use crate::{Context, constants::COIN_EMOJI_MAPPING};
 /// Random Playground
 ///
 /// A list of playful commands that utilse randomization, such as dice rolls, coin flips, etc.
-#[poise::command(slash_command, subcommands("dice", "coin"))]
+#[poise::command(slash_command, subcommands("dice", "coin", "choose"))]
 pub async fn random(_ctx: Context<'_>) -> Result<(), anyhow::Error> {
     Ok(())
 }
@@ -60,5 +60,26 @@ pub async fn coin(ctx: Context<'_>) -> Result<(), anyhow::Error> {
         ..Default::default()
     })
     .await?;
+    Ok(())
+}
+
+/// Choose randomly between multiple options provided by the user.
+///
+////// This command allows users to input multiple options, and the bot will randomly select one of them. It is useful for making decisions or adding an element of chance to a conversation.
+#[poise::command(slash_command)]
+pub async fn choose(
+    ctx: Context<'_>,
+    #[description = "Options to choose from"] options: String,
+    #[description = "Saperator for options, default to comma"] separator: Option<String>,
+) -> Result<(), anyhow::Error> {
+    let saperator = separator.unwrap_or(",".to_string());
+    let options: Vec<&str> = options.split(&saperator).map(|s| s.trim()).collect();
+    if options.len() < 2 {
+        ctx.say("Please provide at least two options separated by commas.")
+            .await?;
+        return Ok(());
+    }
+    let choice = options[(rand::random::<u32>() % options.len() as u32) as usize];
+    ctx.say(format!("I choose: {}", choice)).await?;
     Ok(())
 }
