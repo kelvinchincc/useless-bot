93ae230ed19561b4be80e128069bd97812369340
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-08-16 21:33:45 +0800

    Reorganized imports

--- src/utils/link_helper.rs
+++ src/utils/link_helper.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
-
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+use std::collections::HashMap;
+
 use anyhow::{Context, Result};
 use html5ever::{parse_document, tendril::TendrilSink};
 use markup5ever_rcdom::{Handle, NodeData, RcDom};
